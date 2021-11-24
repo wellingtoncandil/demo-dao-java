897b672a1942ea4429ea1ca02eeb4a78fa819afc
Wellington Candil <wcandil34@gmail.com>
2021-11-24 11:21:14 -0300

    Reusando instanciações de Seller e Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,19 +10,14 @@ import model.entities.Seller;
 public class Program {
 	public static void main(String[] args) {
 		
-		Department dep = new Department(2, "books");
-		System.out.println(dep);
-		
-		Seller seller = new Seller(2, "Well", "wcandil@gmail", new Date(), 4500.0, dep);
 		
 		@SuppressWarnings("unused")
 		SellerDao sellerDao = DaoFactory.createSellerDao();//desta forma o programa não conhece a implementação (conhece somente a interface)
 		//também é uma forma de se fazer uma injeção de dependencia sem deixar explicita a implementação
+		
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
-		Seller seller2 = sellerDao.findById(3);
-		
-		System.out.println(seller2);
 	}
 
 }
