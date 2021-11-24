1b4134964f6d3b9a44bad98c875ff2d53bfbc1a6
Wellington Candil <wcandil34@gmail.com>
2021-11-24 11:09:43 -0300

    implementação do findById para sellers

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.implement.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,10 +15,14 @@ public class Program {
 		
 		Seller seller = new Seller(2, "Well", "wcandil@gmail", new Date(), 4500.0, dep);
 		
+		@SuppressWarnings("unused")
 		SellerDao sellerDao = DaoFactory.createSellerDao();//desta forma o programa não conhece a implementação (conhece somente a interface)
 		//também é uma forma de se fazer uma injeção de dependencia sem deixar explicita a implementação
 		System.out.println(seller);
 		
+		Seller seller2 = sellerDao.findById(3);
+		
+		System.out.println(seller2);
 	}
 
 }
