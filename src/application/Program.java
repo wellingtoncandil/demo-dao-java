fab765d14e54b6287a0eb95def4c93a841dde62a
Wellington Candil <wcandil34@gmail.com>
2021-11-24 10:28:31 -0300

    criação das interfaces e implementação do SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,9 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.implement.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +16,8 @@ public class Program {
 		
 		Seller seller = new Seller(2, "Well", "wcandil@gmail", new Date(), 4500.0, dep);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();//desta forma o programa não conhece a implementação (conhece somente a interface)
+		//também é uma forma de se fazer uma injeção de dependencia sem deixar explicita a implementação
 		System.out.println(seller);
 		
 	}
