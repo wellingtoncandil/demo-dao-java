a4b46d8d0eb4248d644edbe63e6a40d462b5ee94
Wellington Candil <wcandil34@gmail.com>
2021-11-24 15:29:15 -0300

    implementação delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
@@ -48,7 +50,7 @@ public class Program {
 		Date date = new Date();
 		date = sdf.parse("13/12/1991");
 		Seller seller2 = new Seller(null, "Well", "well@gmail.com", date, 3800.0, department);
-		sellerDao.insert(seller2);
+		//sellerDao.insert(seller2);
 		System.out.println("Inserted! New seller id = " + seller2.getId());
 		
 		System.out.println("\n=== TEST 5: seller update ===");
@@ -56,6 +58,14 @@ public class Program {
 		seller.setName("Chris");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 5: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("User deleted");
+		
+		sc.close();
 		
 	}
 }
